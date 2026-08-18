c34b3b9ba259ea1e0417efda9d720effa94a6676
skigcbw <skigcbw@gmail.com>
2026-08-18 13:27:15 +0200

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,13 +1,8 @@
-import noirAsset from "@/assets/reklamacja47.jpg.asset.json";
-const noir = noirAsset.url;
-import monoAsset from "@/assets/niema-faf.jpg.asset.json";
-const mono = monoAsset.url;
-import chromeAsset from "@/assets/phamu-skille2.jpg.asset.json";
-const chrome = chromeAsset.url;
-import formAsset from "@/assets/fefe.jpg.asset.json";
-const form = formAsset.url;
-import voidImg from "@/assets/project-void.jpg";
-import moodboard from "@/assets/detail-moodboard.jpg";
+import noir from "@/assets/reklamacja47.jpg";
+import mono from "@/assets/niema-faf.jpg";
+import chrome from "@/assets/phamu-skille2.jpg";
+import form from "@/assets/fefe.jpg";
+
 
 export type Project = {
   slug: string;
